a51df602fdd75d234d61fc595e4d7cf6c4e0dbd8
QuanDH <rickymta@gmail.com>
2023-11-18 23:19:50 +0700

    Session 07

---
+++ Session07/scripts/SQLQuery1.sql
@@ -0,0 +1,50 @@
+﻿-- tạo database Demo
+create database Demo
+go
+
+-- sử dụng database Demo
+use Demo
+go
+
+-- tạo bảng Employees
+create table Employees (
+	Id int primary key identity,
+	Name nvarchar(50),
+	Role nvarchar(50)
+)
+go
+
+-- thêm dữ liệu vào bảng Employees
+insert into Employees values
+(N'Nguyễn Văn A', N'PM'),
+(N'Nguyễn Văn B', N'JQC'),
+(N'Nguyễn Văn C', N'BrSE'),
+(N'Nguyễn Văn D', N'Dev'),
+(N'Nguyễn Văn E', N'QC');
+
+-- lấy toàn bộ dữ liệu từ bảng Employees
+select * from Employees
+
+-- cập nhật dữ liệu
+update Employees set Name = N'Dương Trung Đạt' where Id = 1 --(unicode)
+
+update
+	Employees
+set
+	Name = N'Phạm Thị Huyền',
+	Role = N'SubPM'
+where
+	Id = 3
+
+-- xoá dữ liệu
+delete from Employees where Id = 2
+
+-- SQL script (CRUD)
+--insert (C-Create)
+--select (R-Read)
+--update (U-Update)
+--delete (D-Delete)
+-- <table_name>
+
+-- câu lệnh select:
+select top 3 Name Ten, Role ChucVu from Employees
---
+++ Session07/scripts/SQLQuery2.sql
@@ -0,0 +1,53 @@
+﻿-- Relational
+create table Departments (
+	Id int primary key identity,
+	Name nvarchar(50),
+	Code varchar(50) not null unique,
+	Status bit not null default 1 -- (boolean | bit sẽ nhận 2 giá trị: 1, 0 tương ứng với true và false)
+)
+go
+
+drop table Employees;
+create table Employees (
+	Id int primary key identity,
+	Name nvarchar(50),
+	Role nvarchar(50),
+	DepartmentId int foreign key references Departments(Id)
+)
+go
+
+insert into Departments values
+(N'Đào tạo', 'DT', 1),
+(N'Hành Chính', 'HC', 0),
+(N'Phát triển sản phẩm', 'PTSP', 1);
+
+insert into Employees values
+(N'Dương Trung Đạt', 'Manager', 3),
+(N'Phạm Thị Huyền', 'BrSE', 3),
+(N'Nghiêm Phương Anh', 'QA', 1),
+(N'Nguyễn Tiến Đông', N'Lễ tân', 2),
+(N'Nguyễn Văn Khánh', 'IT', 2),
+(N'Nguyễn Văn Đạt', 'IT', 2),
+(N'Nguyễn Đức Cường', 'Dev', 3);
+
+select * from Departments
+select * from Employees
+
+select * from Departments inner join Employees on Departments.Id = Employees.DepartmentId
+
+select
+	e.Id MaNV,
+	e.Name HoVaTen,
+	e.Role ChucVu,
+	d.Name TenPhongBan,
+	d.Status TrangThaiPhongBan
+from
+	Departments d
+inner join
+	Employees e
+on
+	d.Id = e.DepartmentId
+
+
+insert into Departments values
+('', null, 1); --'' string.IsNullOrWhiteSpace() | string.IsNullOrEmpty()
